42e6d4825fe4a99eae9d730cf348a474b9df93b4
allanhull <allanpaulhull@outlook.com>
2020-07-20 21:05:36 -0500

    2020-UUL-20: Commit #1

--- app/src/main/java/com/allanhull/crom/StoresFragment.kt
+++ app/src/main/java/com/allanhull/crom/StoresFragment.kt
@@ -1,10 +1,18 @@
 package com.allanhull.crom
 
+
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
+import android.widget.ProgressBar
+import okhttp3.*
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.IOException
+import kotlin.collections.ArrayList
 import com.allanhull.crom.R
 
 
@@ -13,6 +21,10 @@ import com.allanhull.crom.R
  */
 class StoresFragment : Fragment() {
 
+    lateinit var progress:ProgressBar
+    lateinit var listView_details: ListView
+    var arrayList_details:ArrayList<StoreModel> = ArrayList()
+    val client = OkHttpClient()
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
